3ade9d726ffb4d50166a5ca322caf4065fff1a9b
aligero <jobforajoy@gmail.com>
2020-08-30 15:20:10 +0300

    Initial commit

---
+++ src/main/java/ru/alina/profileeditor/ProfileEditorApplication.java
@@ -0,0 +1,13 @@
+package ru.alina.profileeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileEditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/alina/profileeditor/ProfileEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alina.profileeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
